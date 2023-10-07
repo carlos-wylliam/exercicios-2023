d3dcd27d1dff6d66aa0c705d8347692335ec47c0
DESKTOP-1SK3R1N\Wylliam <carloswylliam023@gmail.com>
2023-10-07 15:26:10 -0300

    criei um metodo pra capturar todos os ids dos artigos existentes

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -16,6 +16,15 @@ class Main {
 
     $data = (new Scrapper())->scrap($dom);
 
+    $idsArray = [];
+        $id = $dom->getElementsByTagName('div');
+        foreach ($id as $ids) {
+            if ($ids->getAttribute('class') === 'volume-info') {
+                $idsArray[] = $ids->textContent;
+            }
+        }
+        print_r($idsArray);
+
     // Write your logic to save the output file bellow.
     print_r($data);
   }
