937628da3dfb7b49141901097918f1d274debeb9
DESKTOP-1SK3R1N\Wylliam <carloswylliam023@gmail.com>
2023-10-07 16:19:16 -0300

    correção

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -7,19 +7,18 @@ error_reporting(E_ERROR | E_PARSE);
 /**
  * Runner for the Webscrapping exercice.
  */
-class Main{
+class Main {
 
     /**
      * Main runner, instantiates a Scrapper and runs.
      */
-    public static function run(): void{
-        $dom = new \DOMDocument('1.0', 'utf-8');
-        $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
+    public static function run(): void {
+    $dom = new \DOMDocument('1.0', 'utf-8');
+    $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
 
-        $data = (new Scrapper())->scrap($dom);
+    $data = (new Scrapper())->scrap($dom);
 
-        // Write your logic to save the output file bellow.
-        print_r($data);
-    }
-    
+    // Write your logic to save the output file bellow.
+    print_r($data);
+    }   
 }
