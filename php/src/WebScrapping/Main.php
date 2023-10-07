18bf444dcb925ba692e5546715e839890ef0cb8d
DESKTOP-1SK3R1N\Wylliam <carloswylliam023@gmail.com>
2023-10-07 15:26:47 -0300

    implementacao: capturar os ids

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -13,15 +13,6 @@ class Main {
   public static function run(): void {
     $dom = new \DOMDocument('1.0', 'utf-8');
     $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
-
-    $data = (new Scrapper())->scrap($dom);
-
-    $idsArray = [];
-        $id = $dom->getElementsByTagName('div');
-        foreach ($id as $ids) {
-            if ($ids->getAttribute('class') === 'volume-info') {
-                $idsArray[] = $ids->textContent;
-            }
         }
         print_r($idsArray);
 
