eab9ac761f95d28b0cbdc578d90d92c3931f2eab
carlos-wylliam <carloswylliam023@gmail.com>
2024-01-17 19:01:03 -0300

    indented incorrectly

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -9,48 +9,45 @@ use DOMXPath;
 /**
  * Does the scrapping of a webpage.
  */
-class Scrapper
-{
+class Scrapper {
 
-    /**
-     * Loads paper information from the HTML and returns the array with the data.
-     */
-    public function scrap(\DOMDocument $dom): array
-    {
-  
-        $xpath = new DOMXPath($dom);
+  /**
+   * Loads paper information from the HTML and returns the array with the data.
+   */
+  public function scrap(\DOMDocument $dom): array {
+    $xpath = new DOMXPath($dom);
 
-        $papers = [];
+    $papers = [];
 
-        foreach ($xpath->query('//a[contains(@class, "paper-card")]') as $value) {
-            $id = $xpath->query('div/div/div[contains(@class, "volume-info")]', $value)->item(0)->textContent;
-            $title = $xpath->query('h4', $value)->item(0)->textContent;
-            $type = $xpath->query('div/div[contains(@class, "tags mr-sm")]', $value)->item(0)->textContent;
+    foreach ($xpath->query('//a[contains(@class, "paper-card")]') as $value) {
+      $id = $xpath->query('div/div/div[contains(@class, "volume-info")]', $value)->item(0)->textContent;
+      $title = $xpath->query('h4', $value)->item(0)->textContent;
+      $type = $xpath->query('div/div[contains(@class, "tags mr-sm")]', $value)->item(0)->textContent;
     
-            $persons = [];
+      $persons = [];
 
-            foreach ($xpath->query('div[contains(@class, "authors")]/span', $value) as $authorSpan) {
-                $institution = $authorSpan->getAttribute('title');
-                $authorName = $authorSpan->textContent;
-                $persons[] = new Person($authorName, $institution);
-            }
+    foreach ($xpath->query('div[contains(@class, "authors")]/span', $value) as $authorSpan) {
+      $institution = $authorSpan->getAttribute('title');
+      $authorName = $authorSpan->textContent;
+      $persons[] = new Person($authorName, $institution);
+  }
 
-            $papers[] = new Paper($id, $title, $type, $persons);
-        }
+    $papers[] = new Paper($id, $title, $type, $persons);
+  }
 
-        return $papers;
+//   return $papers;
   
-        return [
-        new Paper(
-            123,
-            'The Nobel Prize in Physiology or Medicine 2023',
-            'Nobel Prize',
-            [
-            new Person('Katalin Karikó', 'Szeged University'),
-            new Person('Drew Weissman', 'University of Pennsylvania'),
-            ]
-        ),
-        ];
-    }
+//         return [
+//         new Paper(
+//             123,
+//             'The Nobel Prize in Physiology or Medicine 2023',
+//             'Nobel Prize',
+//             [
+//             new Person('Katalin Karikó', 'Szeged University'),
+//             new Person('Drew Weissman', 'University of Pennsylvania'),
+//             ]
+//         ),
+//         ];
+//     }
 
 }
