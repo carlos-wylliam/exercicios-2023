1b1bf96f6ef5caa69d4bc21367b89dc837a04010
carlos-wylliam <carloswylliam023@gmail.com>
2024-01-17 18:56:32 -0300

    indented incorrectly

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -13,8 +13,8 @@ error_reporting(E_ERROR | E_PARSE);
 class Main {
 
   /**
-    * Main runner, instantiates a Scrapper and runs.
-    */
+   * Main runner, instantiates a Scrapper and runs.
+   */
   public static function run(): void {
     $dom = new \DOMDocument('1.0', 'utf-8');
     $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
@@ -27,13 +27,13 @@ class Main {
     $writer->openToFile('./assets/model.xlsx');
 
     $header = ['Id', 'Titulo', 'Tipo',
-    'Autor 1', 'Autor 1 Instituição', 'Autor 2', 'Autor 2 Instituição', 'Autor 3', 'Autor 3 Instituicao',
-    'Autor 4', 'Autor 4 Instituição', 'Autor 5', 'Autor 5 Instituição', 'Autor 6', 'Autor 6 Instituição',
-    'Autor 7', 'Autor 7 Instituição', 'Autor 8', 'Autor 8 Instituição', 'Autor 9', 'Autor 9 Instituição',
-    'Autor 10', 'Autor 10 Instituição', 'Autor 11', 'Autor 11 Instituição', 'Autor 12', 'Autor 12 Instituição',
-    'Autor 13', 'Autor 13 Instituição', 'Autor 14', 'Autor 14 Instituição', 'Autor 15', 'Autor 15 Instituição',
-    'Autor 16', 'Autor 16 Instituição', 'Autor 17', 'Autor 17 Instituição', 'Autor 18', 'Autor 18 Instituição',
-];
+      'Autor 1', 'Autor 1 Instituição', 'Autor 2', 'Autor 2 Instituição', 'Autor 3', 'Autor 3 Instituicao',
+      'Autor 4', 'Autor 4 Instituição', 'Autor 5', 'Autor 5 Instituição', 'Autor 6', 'Autor 6 Instituição',
+      'Autor 7', 'Autor 7 Instituição', 'Autor 8', 'Autor 8 Instituição', 'Autor 9', 'Autor 9 Instituição',
+      'Autor 10', 'Autor 10 Instituição', 'Autor 11', 'Autor 11 Instituição', 'Autor 12', 'Autor 12 Instituição',
+      'Autor 13', 'Autor 13 Instituição', 'Autor 14', 'Autor 14 Instituição', 'Autor 15', 'Autor 15 Instituição',
+      'Autor 16', 'Autor 16 Instituição', 'Autor 17', 'Autor 17 Instituição', 'Autor 18', 'Autor 18 Instituição',
+    ];
     $headerRow = WriterEntityFactory::createRowFromArray($header);
 
     $writer->addRow($headerRow);
@@ -47,15 +47,15 @@ class Main {
         ]
         );
 
-        foreach ($paper->authors as $i => $author) {
-          $row->addCell(WriterEntityFactory::createCell($author->name));
-          $row->addCell(WriterEntityFactory::createCell($author->institution));
-  }
+      foreach ($paper->authors as $i => $author) {
+        $row->addCell(WriterEntityFactory::createCell($author->name));
+        $row->addCell(WriterEntityFactory::createCell($author->institution));
+      }
 
-    $writer->addRow($row);
-  }
+      $writer->addRow($row);
+    }
     $writer->close();
     // // Write your logic to save the output file below.
-    }
+  }
 
 }
