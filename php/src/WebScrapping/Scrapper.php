a837a8915df16b197f78421d57b2cf5035515ec4
carlos-wylliam <carloswylliam023@gmail.com>
2024-03-13 22:51:58 -0300

    fix: lint

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -14,17 +14,16 @@ class Scrapper {
   /**
    * Loads paper information from the HTML and returns the array with the data.
    */
-    public function scrap(\DOMDocument $dom): array
-    {
-        $xpath = new DOMXPath($dom);
+  public function scrap(\DOMDocument $dom): array {
+    $xpath = new DOMXPath($dom);
 
-        $papers = [];
+    $papers = [];
 
-        foreach ($xpath->query('//a[contains(@class, "paper-card")]') as $value) {
-            $id = $xpath->query('div/div/div[contains(@class, "volume-info")]', $value)->item(0)->textContent;
-            $title = $xpath->query('h4', $value)->item(0)->textContent;
-            $type = $xpath->query('div/div[contains(@class, "tags mr-sm")]', $value)->item(0)->textContent;
-            $persons = [];
+    foreach ($xpath->query('//a[contains(@class, "paper-card")]') as $value) {
+      $id = $xpath->query('div/div/div[contains(@class, "volume-info")]', $value)->item(0)->textContent;
+      $title = $xpath->query('h4', $value)->item(0)->textContent;
+      $type = $xpath->query('div/div[contains(@class, "tags mr-sm")]', $value)->item(0)->textContent;
+      $persons = [];
 
             foreach ($xpath->query('div[contains(@class, "authors")]/span', $value) as $authorSpan) {
                 $institution = $authorSpan->getAttribute('title');
