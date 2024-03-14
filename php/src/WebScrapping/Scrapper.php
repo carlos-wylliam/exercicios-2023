f7f5cad5b6ffc2a1ae5cc350eb7c3af1e9916187
carlos-wylliam <carloswylliam023@gmail.com>
2024-03-13 22:38:27 -0300

    phpcbf

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -9,32 +9,34 @@ use DOMXPath;
 /**
  * Does the scrapping of a webpage.
  */
-class Scrapper {
+class Scrapper
+{
 
-  /**
-   * Loads paper information from the HTML and returns the array with the data.
-   */
-  public function scrap(\DOMDocument $dom): array {
-    $xpath = new DOMXPath($dom);
+    /**
+     * Loads paper information from the HTML and returns the array with the data.
+     */
+    public function scrap(\DOMDocument $dom): array
+    {
+        $xpath = new DOMXPath($dom);
 
-    $papers = [];
+        $papers = [];
 
-    foreach ($xpath->query('//a[contains(@class, "paper-card")]') as $value) {
-      $id = $xpath->query('div/div/div[contains(@class, "volume-info")]', $value)->item(0)->textContent;
-      $title = $xpath->query('h4', $value)->item(0)->textContent;
-      $type = $xpath->query('div/div[contains(@class, "tags mr-sm")]', $value)->item(0)->textContent;
-      $persons = [];
+        foreach ($xpath->query('//a[contains(@class, "paper-card")]') as $value) {
+            $id = $xpath->query('div/div/div[contains(@class, "volume-info")]', $value)->item(0)->textContent;
+            $title = $xpath->query('h4', $value)->item(0)->textContent;
+            $type = $xpath->query('div/div[contains(@class, "tags mr-sm")]', $value)->item(0)->textContent;
+            $persons = [];
 
-      foreach ($xpath->query('div[contains(@class, "authors")]/span', $value) as $authorSpan) {
-        $institution = $authorSpan->getAttribute('title');
-        $authorName = $authorSpan->textContent;
-        $persons[] = new Person($authorName, $institution);
-      }
+            foreach ($xpath->query('div[contains(@class, "authors")]/span', $value) as $authorSpan) {
+                $institution = $authorSpan->getAttribute('title');
+                $authorName = $authorSpan->textContent;
+                $persons[] = new Person($authorName, $institution);
+            }
       
-      $papers[] = new Paper($id, $title, $type, $persons);
-    }
+            $papers[] = new Paper($id, $title, $type, $persons);
+        }
 
-    return $papers;
+        return $papers;
   
         return [
         new Paper(
